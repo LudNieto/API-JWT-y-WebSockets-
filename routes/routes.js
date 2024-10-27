60de33b03571cdfc8d5533106266f3b7b989d290
Ludwing Nieto <ldnietob@gmail.com>
2024-10-27 15:09:46 -0500

    Add route for Socket-test

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { register, login } = require('../controllers/authController');
 const authenticateToken = require('../middlewares/authMiddleware');
 const router = express.Router();
@@ -8,6 +9,10 @@ router.post('/login', login);
 
 router.get('/protected', authenticateToken, (req, res) => {
     res.json({ message: 'Esta es una ruta protegida', user: req.user });
+});
+
+router.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html')); // Ruta al archivo index.html
 });
 
 module.exports = router;
